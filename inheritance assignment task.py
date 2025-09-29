f8cb8f225fd807d49f41bf3826479c8aad77045e
Rohit-specs <rohit5592006@gmail.com>
2025-09-29 20:25:20 +0530

    added motorcycle class

--- inheritance assignment task.py
+++ inheritance assignment task.py
@@ -8,12 +8,20 @@ class Vehicle:
         print("Model:",self.model)
         print("Year:",self.year)
 class Car(Vehicle):
-    def init(self,make,model,year):
-        super().__init(make,model,year)
+    def __init__(self,make,model,year):
+        super().__init__(make,model,year)
+    def display_info(self):
+        super().display_info()
+class Motorcycle(Vehicle):
+    def __init__(self,make,model,year):
+        super().__init__(make,model,year)
     def display_info(self):
         super().display_info()
 
 obj=Car("Toyota","don't know about models",2022)
+obj.display_info()
+print()
+obj=Motorcycle("Splender","What are models? this is from future ",2066)
 obj.display_info()
 
 
