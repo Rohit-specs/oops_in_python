d751b891cf5a0dea1c1c68e9ebd618ceec6ca53c
Rohit-specs <rohit5592006@gmail.com>
2025-09-29 21:24:24 +0530

    assignment task of abstraction basic

---
+++ abstraction basic assignment.py
@@ -0,0 +1,20 @@
+from abc import ABC,abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        print("THis will print area")
+class Rectangle(Shape):
+    def area(self,l,b):
+        print("Area of Rectangle: ",l*b)
+class Circle(Shape):
+    def area(self,r):
+        print("Area of circle: ",(22/7)*r**2)
+
+obj=Rectangle()
+obj.area(8,5)
+
+print()
+
+obj1=Circle()
+obj1.area(7)
